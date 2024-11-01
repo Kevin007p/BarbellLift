6c14b1d797f977852ecf3b5fac54f5c42fe946ba
Kevin007p <kkevinpatel07@gmail.com>
2024-11-01 01:13:12 -0400

    first commit

---
+++ src/features/TemporalAbstraction.py
@@ -0,0 +1,34 @@
+import numpy as np
+import scipy.stats as stats
+
+# Class to abstract a history of numerical values we can use as an attribute.
+class NumericalAbstraction:
+
+    def aggregate_value(self, aggregation_function):
+        # Compute the values and return the result.
+        if aggregation_function == "mean":
+            return np.mean
+        elif aggregation_function == "max":
+            return np.max
+        elif aggregation_function == "min":
+            return np.min
+        elif aggregation_function == "median":
+            return np.median
+        elif aggregation_function == "std":
+            return np.std
+        else:
+            return np.nan
+
+    def abstract_numerical(self, data_table, cols, window_size, aggregation_function):
+
+        # Create new columns for the temporal data, pass over the dataset and compute values
+        for col in cols:
+            data_table[
+                col + "_temp_" + aggregation_function + "_ws_" + str(window_size)
+            ] = (
+                data_table[col]
+                .rolling(window_size)
+                .apply(self.aggregate_value(aggregation_function))
+            )
+
+        return data_table
